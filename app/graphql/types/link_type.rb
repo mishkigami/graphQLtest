7862fff4e28944e22d162259d03d2b0930d9dac3
mishkigami <mihail.kodolov@gmail.com>
2020-06-19 22:05:40 +0200

    add mutations+Auth

---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class LinkType < Types::BaseObject
+    field :id, ID, null: false
+    field :url, String, null: false
+    field :description, String, null: false
+    field :posted_by, UserType, null: true, method: :user
+  end
+end
